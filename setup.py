1e65e4676b3b24bb621132df3130d6c1311db85c
Csaba Palankai <csaba.palankai@gmail.com>
2015-08-29 13:17:07 +0100

    Fix versioning, add funcional tests skeleton

--- setup.py
+++ setup.py
@@ -3,38 +3,42 @@ import os
 
 from setuptools import setup, find_packages
 
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(
-    name='pyrs-resource',
-    author='Csaba Palankai',
-    author_email='csaba.palankai@gmail.com',
-    packages=find_packages(exclude=['*.tests', 'tests', '*.tests.*']),
-    include_package_data=True,
-    version='0.2.0',
-    description="Python microservice framework",
-    url='https://github.com/palankai/pyrs-resource',
-    long_description=read('README.rst'),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords=('service', 'rest', 'restful', 'swagger', 'resource'),
-    zip_safe=False,
-    install_requires=[r for r in read("requirements.txt").split("\n") if r],
-)
+VERSION = '0.3.0'
+
+if __name__ == '__main__':
+    setup(
+        name='pyrs-resource',
+        author='Csaba Palankai',
+        author_email='csaba.palankai@gmail.com',
+        packages=find_packages(exclude=['*.tests', 'tests', '*.tests.*']),
+        include_package_data=True,
+        version=VERSION,
+        description="Python microservice framework",
+        url='https://github.com/palankai/pyrs-resource',
+        long_description=read('README.rst'),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Operating System :: OS Independent',
+            'Topic :: Internet :: WWW/HTTP :: WSGI',
+            'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        keywords=('service', 'rest', 'restful', 'swagger', 'resource'),
+        zip_safe=False,
+        install_requires=[
+            r for r in read("requirements.txt").split('\n') if r
+        ],
+    )
